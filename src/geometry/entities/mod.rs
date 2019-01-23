081303477d3b76d18ffa4a676f5adc5b112a54d8
Niakr1s <pavel2188@gmail.com>
2019-01-23 19:39:33 +0300

    implemented Rect::add_rect()

--- src/geometry/entities/mod.rs
+++ src/geometry/entities/mod.rs
@@ -102,12 +102,35 @@ impl Rect {
         }
     }
 
-    pub fn from(xmax: f64, ymax: f64, xmin: f64, ymin: f64) -> Rect {
-        Rect { xmax, ymax, xmin, ymin, empty: false }
+    pub fn from(xmax: f64, ymax: f64, xmin: f64, ymin: f64) -> Result<Rect, String> {
+        let r = Rect { xmax, ymax, xmin, ymin, empty: false };
+        if xmax < xmin || ymax < ymin {
+            return Err(format!("rect {:?} is wrong, max value less then min", r))
+        };
+        Ok(Rect { xmax, ymax, xmin, ymin, empty: false })
     }
 
     pub fn is_empty(&self) -> bool {
         self.empty
+    }
+
+    pub fn add_rect(&mut self, other: &Rect) {
+        if other.is_empty() { return };
+        if self.is_empty() {
+            *self = other.clone();
+        }
+        if other.xmax > self.xmax {
+            self.xmax = other.xmax
+        };
+        if other.ymax > self.ymax {
+            self.ymax = other.ymax
+        };
+        if other.xmin < self.xmin {
+            self.xmin = other.xmin
+        };
+        if other.ymin < self.ymin {
+            self.ymin = other.ymin
+        };
     }
 
     /// returns true if self has changed
@@ -115,7 +138,7 @@ impl Rect {
         match r {
             Some(r) => {
                 if self.is_empty() {
-                    *self = Rect::from(x+r, y+r, x-r, y-r);
+                    *self = Rect::from(x+r, y+r, x-r, y-r).unwrap();
                     return;
                 };
                 if x+r > self.xmax { self.xmax = x+r};
@@ -125,13 +148,19 @@ impl Rect {
             },
             None => {
                 if self.is_empty() {
-                    *self = Rect::from(x, y, x, y);
+                    *self = Rect::from(x, y, x, y).unwrap();
                     return;
                 };
-                if x > self.xmax { self.xmax = x};
-                if x < self.xmin { self.xmin = x};
-                if y > self.ymax { self.ymax = y};
-                if y < self.ymin { self.ymin = y};
+                if x > self.xmax {
+                    self.xmax = x
+                } else if x < self.xmin {
+                    self.xmin = x
+                };
+                if y > self.ymax {
+                    self.ymax = y
+                } else if y < self.ymin {
+                    self.ymin = y
+                };
             },
         }
 

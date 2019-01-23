73db3a1257dbd82b5521c7df9803dcf9e4622de9
Niakr1s <pavel2188@gmail.com>
2019-01-23 21:34:29 +0300

    add fn rects_can_not_intersect and tests for it

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -1,12 +1,18 @@
 use crate::mydxf::MyDxf;
 use crate::rrxml::RrXml;
 use crate::rrxml::Parcel;
+use crate::geometry::rect::Rectangable;
 
 pub mod simple_checks;
 pub mod entities;
 pub mod rect;
 
-
+/// If true - rects can not intersect physically
+/// If false - rects can both intersect or not
+fn rects_can_not_intersect(a: &impl Rectangable, b: &impl Rectangable) -> bool {
+    let (a, b) = (a.rect(), b.rect());
+    a.xmax < b.xmin || a.ymax < b.ymin || a.xmin > b.xmax || a.ymin > b.ymax
+}
 
 fn check_mydxf_in_rrxml(mydxf: &MyDxf, rrxml: &RrXml) -> Option<Vec<Parcel>> {
     if rrxml.len() == 0 { return None };
@@ -33,3 +39,6 @@ fn check_mydxf_in_parcel(mydxf: &MyDxf, parcel: &Parcel) -> bool {
 
     unimplemented!()
 }
+
+#[cfg(test)]
+mod test;
---
+++ src/geometry/test.rs
@@ -0,0 +1,20 @@
+use crate::geometry::entities::Contur;
+use crate::geometry::entities::Point;
+use super::*;
+
+#[test]
+fn rects_can_not_intersect_ok() {
+    let p = Point::new(1., 2., Some(3.));  // got Rect {4 5 -2 -1}
+
+    let mut c = Contur::new();
+    c.add(Point::new(2., 3., None));
+    c.add(Point::new(-2., -3., None));
+    c.add(Point::new(4., 2., None));
+    // got Rect { 4 3 -2 -3 }
+
+    assert!(!rects_can_not_intersect(&p, &c));
+
+    let p = Point::new(5., 0., None);  // got Rect {5 0 5 0}
+
+    assert!(rects_can_not_intersect(&p, &c));
+}

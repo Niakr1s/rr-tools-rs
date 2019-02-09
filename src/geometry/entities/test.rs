5c9eb890cb61a96521eb44c9241db6952a077677
Niakr1s <pavel2188@gmail.com>
2019-02-09 13:19:19 +0300

    impl relate_entities (maybe only for Entity for a time being)

--- src/geometry/entities/test.rs
+++ src/geometry/entities/test.rs
@@ -1,6 +1,8 @@
 use crate::geometry::traits::relative::*;
 use super::*;
 
+/// For tests with fig* at the end, see drawing from folder \test_files\explanation\
+
 #[test]
 fn entity_relate_entity_fig1() {
     let point = Entity::from_point(Point::new(0., 0., None));
@@ -121,4 +123,77 @@ fn intersect_switch_ok() {
     let mut intersect = false;
     intersect_switch(&mut intersect, true);
     assert_eq!(intersect, true);
+}
+
+#[test]
+fn relate_entities_fig3() {
+    let outer = Entity::from_contur(contur![
+        Point::new(-9.,-9.,None),
+        Point::new(-9.,8.,None),
+        Point::new(10.,10.,None),
+        Point::new(11.,-6.,None),
+        Point::new(-4.,-12.,None),
+        Point::new(-9.,-9.,None)
+    ]).unwrap();
+    let inner1 = Entity::from_contur(contur![
+        Point::new(-6.,0.,None),
+        Point::new(-9.,8.,None),
+        Point::new(-2.,0.,None),
+        Point::new(3.,-6.,None),
+        Point::new(-5.,-7.,None),
+        Point::new(-6.,0.,None)
+    ]).unwrap();
+    let inner1_inner = Entity::from_contur(contur![
+        Point::new(-4.,-2.,None),
+        Point::new(-2.,-1.,None),
+        Point::new(0.,-5.,None),
+        Point::new(-4.,-5.,None),
+        Point::new(-4.,-2.,None)
+    ]).unwrap();
+    let inner2 = Entity::from_contur(contur![
+        Point::new(3.,2.,None),
+        Point::new(7.,2.,None),
+        Point::new(9.,-1.,None),
+        Point::new(4.,-3.,None),
+        Point::new(3.,2.,None)
+    ]).unwrap();
+    let entities: Entities = vec![outer, inner1, inner1_inner, inner2];
+
+    let red_line = Entity::from_contur(contur![
+        Point::new(-3.,-3.,None),
+        Point::new(-2.,-3.,None),
+        Point::new(-2.,-4.,None),
+        Point::new(-3.,-4.,None)
+    ]).unwrap();
+
+    assert_eq!(red_line.relate_entities(&entities), Some(Relation::Inside));
+
+    let green_line = Entity::from_contur(contur![
+        Point::new(-1.,-6.,None),
+        Point::new(-4.,-6.,None),
+        Point::new(-5.,-4.,None),
+        Point::new(-5.,-1.,None),
+        Point::new(-3.,-1.,None)
+    ]).unwrap();
+
+    assert_eq!(green_line.relate_entities(&entities), None);
+
+    let red_circle = Entity::from_point(Point::new(-2.5, -3.5, Some(1.)));
+
+    assert_eq!(red_circle.relate_entities(&entities), Some(Relation::Inside));
+
+    let blue_circle = Entity::from_point(Point::new(6., 0., Some(4.)));
+
+    assert_eq!(blue_circle.relate_entities(&entities), Some(Relation::Intersect));
+
+    let green_circle = Entity::from_point(Point::new(6., 0., Some(1.)));
+
+    assert_eq!(green_circle.relate_entities(&entities), None);
+
+    let blue_line = Entity::from_contur(contur![
+        Point::new(6.,-3.,None),
+        Point::new(6.,-2.,None)
+    ]).unwrap();
+
+    assert_eq!(blue_line.relate_entities(&entities), Some(Relation::Intersect));
 }
--- src/geometry/traits/relative/mod.rs
+++ src/geometry/traits/relative/mod.rs
@@ -1,14 +1,27 @@
-use crate::geometry::entities::{Entities, Entity};
+use crate::geometry::entities::*;
 use crate::geometry::traits::rectangable::Rectangable;
 
 pub trait Relative: Rectangable {
     fn relate_entity(&self, entity: &Entity) -> Option<Relation>;
-//    fn relate_entities(&self, entities: &Entities) -> bool {
-//        for e in entities {
-//            if self.relate_entity(e) { return true }  // todo for "hole" entities
-//        };
-//        false
-//    }
+
+    fn relate_entities(&self, entities: &Entities) -> Option<Relation> {
+        let mut in_hole: Option<bool> = None;
+        for e in entities {
+            match self.relate_entity(e) {
+                Some(Relation::Intersect) => return Some(Relation::Intersect),
+                Some(Relation::Inside) => match in_hole {
+                    Some(in_hole_bool) => in_hole = Some(!in_hole_bool),
+                    None => in_hole = Some(false),
+                },
+                None => continue,
+            }
+        };
+        match in_hole {
+            Some(true) => None,
+            Some(false) => Some(Relation::Inside),
+            None => None,
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]

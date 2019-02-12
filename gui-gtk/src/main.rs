36005bd219b48abf3ff66f69c1e167e47ff328c3
Niakr1s <pavel2188@gmail.com>
2019-02-12 22:31:44 +0300

    first impl

--- gui-gtk/src/main.rs
+++ gui-gtk/src/main.rs
@@ -1,80 +1,97 @@
+extern crate gdk;
 extern crate gtk;
-#[macro_use]
-extern crate relm;
-#[macro_use]
-extern crate relm_derive;
+extern crate url;
 
-use relm::{Relm, Update, Widget};
+use gdk::DragAction;
+
 use gtk::prelude::*;
-use gtk::{Window, Inhibit, WindowType};
+use gtk::*;
 
-struct Model {
-    // …
-}
+use url::Url;
 
-#[derive(Msg)]
-enum Msg {
-    // …
-    Quit,
-}
-
-struct Win {
-    // …
-    model: Model,
-    window: Window,
-}
-
-impl Update for Win {
-    // Specify the model used for this widget.
-    type Model = Model;
-    // Specify the model parameter used to init the model.
-    type ModelParam = ();
-    // Specify the type of the messages sent to the update function.
-    type Msg = Msg;
-
-    // Return the initial model.
-    fn model(_: &Relm<Self>, _: ()) -> Model {
-        Model {
+macro_rules! clone {
+    (@param _) => ( _ );
+    (@param $x:ident) => ( $x );
+    ($($n:ident),+ => move || $body:expr) => (
+        {
+            $( let $n = $n.clone(); )+
+            move || $body
         }
-    }
-
-    // The model may be updated when a message is received.
-    // Widgets may also be updated in this function.
-    fn update(&mut self, event: Msg) {
-        match event {
-            Msg::Quit => gtk::main_quit(),
+    );
+    ($($n:ident),+ => move |$($p:tt),+| $body:expr) => (
+        {
+            $( let $n = $n.clone(); )+
+            move |$(clone!(@param $p),)+| $body
         }
-    }
+    );
 }
 
-impl Widget for Win {
-    // Specify the type of the root widget.
-    type Root = Window;
-
-    // Return the root widget.
-    fn root(&self) -> Self::Root {
-        self.window.clone()
-    }
-
-    // Create the widgets.
-    fn view(relm: &Relm<Self>, model: Self::Model) -> Self {
-        // GTK+ widgets are used normally within a `Widget`.
-        let window = Window::new(WindowType::Toplevel);
-
-        // Connect the signal `delete_event` to send the `Quit` message.
-        connect!(relm, window, connect_delete_event(_, _), return (Some(Msg::Quit), Inhibit(false)));
-        // There is also a `connect!()` macro for GTK+ events that do not need a
-        // value to be returned in the callback.
-
-        window.show_all();
-
-        Win {
-            model,
-            window,
+// upgrade weak reference or return
+#[macro_export]
+macro_rules! upgrade_weak {
+    ($x:ident, $r:expr) => {{
+        match $x.upgrade() {
+            Some(o) => o,
+            None => return $r,
         }
-    }
+    }};
+    ($x:ident) => {
+        upgrade_weak!($x, ())
+    };
+}
+
+fn get_selected(treeview: &TreeView) -> Vec<String> {
+    let selection = treeview.get_selection();
+    let (treepaths, treemodel) = selection.get_selected_rows();
+    treepaths
+        .iter()
+        .map(|treepath| {
+            let iter = treemodel.get_iter(&treepath).unwrap();
+            treemodel
+                .get_value(&iter, 0)
+                .get::<String>()
+                .expect("not a string")
+        })
+        .collect()
 }
 
 fn main() {
-    Win::run(()).unwrap();
+    if gtk::init().is_err() {
+        println!("Failed to initialize GTK.");
+        return;
+    }
+
+    let glade_src = include_str!(r"glade\rr-tools-rs.glade");
+    let builder = Builder::new_from_string(glade_src);
+    let window: gtk::Window = builder.get_object("main_window").unwrap();
+
+    let rrxml_treeview: TreeView = builder.get_object("rrxml_view").unwrap();
+    let mydxf_treeview: TreeView = builder.get_object("mydxf_view").unwrap();
+
+    // Configure the text view to accept URI lists from other applications. This allows
+    // dragging files & folders from a file browser program onto the textview.
+    let targets = vec![gtk::TargetEntry::new(
+        "text/uri-list",
+        TargetFlags::OTHER_APP,
+        0,
+    )];
+
+    rrxml_treeview.drag_dest_set(DestDefaults::ALL, &targets, DragAction::COPY);
+    rrxml_treeview.connect_drag_data_received(|w, _, _, _, d, _, _| {
+        println!("d&d recieved");
+        for file in d.get_uris() {
+            let url = Url::parse(&file).expect("bad uri");
+            let path = url.to_file_path();
+            println!("{:?}", path);
+        }
+    });
+
+    window.show_all();
+
+    window.connect_delete_event(move |win, _| {
+        win.destroy();
+        Inhibit(false)
+    });
+
+    gtk::main();
 }
